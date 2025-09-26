a603cd097eeeb8a5d2fbc4ed9ab3442beec65a32
Temmy <olawanletemitayo@gmail.com>
2025-09-26 21:27:38 +0100

    chore: file restructure and metadata tag added to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
 import { TaskManagementInterface } from '@/components/ui/Home/TaskManagementInterface';
 import { Stack } from '@chakra-ui/react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'MKV | Home',
+  description: 'Todo list app with Next.js',
+}
 
 export default function Home() {
   return (
